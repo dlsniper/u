a392bce5655c449824f2d9d885af039a02acd9ef
Florin Patan <florin.patan@youview.com>
2016-04-25 01:43:42 +0100

    Update

--- dbg/3/demo3.go
+++ dbg/3/demo3.go
@@ -6,8 +6,11 @@ import (
 
 func main() {
 	a := "World"
-	b := "World"
-	c := "World"
+	b := int(123)
+	c := struct {
+		Demo  bool
+		Demo2 float32
+	}{Demo: true, Demo2: 3.14}
 	d := "World"
 	e := "World"
 	fmt.Printf("Hello %s %s %s %s %s\n", a, b, c, d, e)
